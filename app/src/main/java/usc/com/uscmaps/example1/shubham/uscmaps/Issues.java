e8a1232bf2f0296d396313742421ddb59bfb8d28
Shubham Gupta <shubh01ster@gmail.com>
2017-02-25 12:37:55 -0800

    Maps working, Recycler to be corrected

---
+++ app/src/main/java/usc/com/uscmaps/example1/shubham/uscmaps/Issues.java
@@ -0,0 +1,15 @@
+package usc.com.uscmaps.example1.shubham.uscmaps;
+
+/**
+ * Created by Shubham on 2/25/17.
+ */
+
+public class Issues {
+
+    /**
+     * The map in the Welcome tab is coming behind the tabs layout
+     *
+     *
+     *
+     */
+}
